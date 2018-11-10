339c047d0961250e090a3e1aaa9ea89c8ed14b93
murilocaruy <42190902+murilocaruy@users.noreply.github.com>
2018-11-10 13:59:31 -0200

    Update views.py

--- arearestrita/views.py
+++ arearestrita/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
+from arearestrita.forms import AtividadeForm, AtividadeVinculadaForm
+from arearestrita.models import Atividade
 
 # Create your views here.
 
@@ -27,3 +29,61 @@ def home_aluno(request):
 @user_passes_test(testa_coordenador)
 def home_coordenador(request):
     return render(request, 'arearestrita/home.html')
+
+'''
+def nova_atividade(request):
+    context = {}
+
+    form = AtividadeForm(request.POST)
+    
+    context['form'] = form
+
+    return render(request, 'arearestrita/novaatividade.html', context)
+'''
+'''
+def nova_atividade(request):
+    context = {}
+
+    form = AtividadeForm(request.POST)
+
+    if form.is_valid():
+        titulo = form.changed_data['titulo']
+        descricao = form.changed_data['descricao']
+        conteudo = form.changed_data['conteudo']
+        tipo = form.changed_data['tipo']
+        extras = form.changed_data['extras']
+        form = AtividadeForm()
+        return redirect('arearestrita/home')
+
+    context["form"] = form
+
+    return render(request, 'arearestrita/novaatividade.html', context)    
+'''
+
+def nova_atividade(request):
+    context = {}
+
+    form = AtividadeForm(request.POST)
+
+    if form.is_valid():
+        form.save()
+        return redirect('arearestrita/home')
+
+    context["form"] = form
+
+    return render(request, 'arearestrita/novaatividade.html', context)    
+
+def vincular_atividade(request):
+    context = {}
+
+    form = AtividadeVinculadaForm(request.POST)
+
+    if form.is_valid():
+        form.save()
+        return redirect('arearestrita/home')
+
+    context["form"] = form
+
+    return render(request, 'arearestrita/vincularatividade.html', context)    
+
+
