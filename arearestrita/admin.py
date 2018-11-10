b89a11356d60257174e8c106861eb90d23c36b21
murilocaruy <42190902+murilocaruy@users.noreply.github.com>
2018-11-10 13:56:38 -0200

    Update admin.py

--- arearestrita/admin.py
+++ arearestrita/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django import forms
+from arearestrita.models import Atividade, AtividadeVinculada, Entrega, Mensagem
 
 # Register your models here.
+
+admin.site.register(Atividade)
+admin.site.register(AtividadeVinculada)
+admin.site.register(Entrega)
+admin.site.register(Mensagem)
