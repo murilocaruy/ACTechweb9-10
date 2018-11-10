6165ec3ca5bdd428767463d3e0b8ba37fc60c70a
murilocaruy <42190902+murilocaruy@users.noreply.github.com>
2018-11-10 13:58:53 -0200

    Update urls.py

--- arearestrita/urls.py
+++ arearestrita/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import home_professor, home_aluno
+from arearestrita import views
 
 app_name = 'arearestrita'
 urlpatterns = [
     path('professor', home_professor, name="home_professor"),
     path('aluno', home_aluno, name="home_aluno"),
+    path('professor/novaatividade', views.nova_atividade, name="novaatividade"),
+    path('professor/vincularatividade', views.vincular_atividade, name="vincularatividade")
 ]
